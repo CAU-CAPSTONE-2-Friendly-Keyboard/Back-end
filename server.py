d7afec2c044efb758cf8149f52a57b749e0731e0
HyunJinNo <nhj9831@naver.com>
2023-03-16 16:35:08 +0900

    코드 수정

--- server.py
+++ server.py
@@ -5,42 +5,7 @@ app = Flask(__name__)
         
 @app.route('/', methods=['GET'])
 def home():
-    return 'succeeded to connect to the server . . .'
-
-# 회원가입시 데이터베이스에 계정 데이터 추가
-@app.route('/sign-up', methods=['POST'])
-def sign_up():
-    if request.method == 'POST':
-        # JSON 형식으로 데이터 받기
-        account_data = request.get_json()
-        id = account_data['id']
-        password = account_data['password']
-        
-        # 데이터베이스에 접근
-        db = pymysql.connect(host='localhost',
-                           port=3306,
-                           user='root',
-                           password='12345',
-                           db='friendly_keyboard_accounts',
-                           charset='utf8')
-
-        # 데이터베이스를 사용하기 위한 cursor을 세팅.
-        cursor = db.cursor()
-        
-        # SQL query 작성
-        # INSERT
-        sql = "INSERT INTO accounts (id, password) VALUES ('%s', '%s')" % (id, password)
-        
-        # SQL query 실행
-        cursor.execute(sql)
-        
-        # 데이터 변화 적용
-        # CREATE 또는 DROP, DELETE, UPDATE, INSERT와 같이
-        # 데이터베이스 내부의 데이터에 영향을 주는 함수의 경우 commit()이 필요함.
-        db.commit()
-        
-        return jsonify({'responseText': 'Signing up is successful!'})
-        
+    return 'Friendly Keyboard Server'
 
 # 특정 아이디가 이미 존재하는지 확인
 @app.route('/get_account', methods=['POST'])
@@ -76,5 +41,39 @@ def get_account():
         else:
             return jsonify({'responseText': "'%s' is already used." % id})
 
+# 회원가입시 데이터베이스에 계정 데이터 추가
+@app.route('/sign-up', methods=['POST'])
+def sign_up():
+    if request.method == 'POST':
+        # JSON 형식으로 데이터 받기
+        account_data = request.get_json()
+        id = account_data['id']
+        password = account_data['password']
+        
+        # 데이터베이스에 접근
+        db = pymysql.connect(host='localhost',
+                           port=3306,
+                           user='root',
+                           password='12345',
+                           db='friendly_keyboard_accounts',
+                           charset='utf8')
+
+        # 데이터베이스를 사용하기 위한 cursor을 세팅.
+        cursor = db.cursor()
+        
+        # SQL query 작성
+        # INSERT
+        sql = "INSERT INTO accounts (id, password) VALUES ('%s', '%s')" % (id, password)
+        
+        # SQL query 실행
+        cursor.execute(sql)
+        
+        # 데이터 변화 적용
+        # CREATE 또는 DROP, DELETE, UPDATE, INSERT와 같이
+        # 데이터베이스 내부의 데이터에 영향을 주는 함수의 경우 commit()이 필요함.
+        db.commit()
+        
+        return jsonify({'responseText': 'Signing up is successful!'})
+        
 if __name__ == '__main__':
     app.run('0.0.0.0', port = 5000)
