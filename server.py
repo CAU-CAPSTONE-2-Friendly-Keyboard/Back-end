8d2d61c48f04cac1c1a5c25fead5b82faea2046e
HyunJinNo <nhj9831@naver.com>
2023-03-17 15:55:32 +0900

    전역 변수 사용 및 로그인 api 작성

--- server.py
+++ server.py
@@ -2,6 +2,17 @@ from flask import Flask, session, render_template, redirect, request, url_for, j
 import pymysql
 
 app = Flask(__name__)
+
+# 데이터베이스에 접근
+db = pymysql.connect(host='localhost',
+                   port=3306,
+                   user='root',
+                   password='12345',
+                   db='friendly_keyboard_accounts',
+                   charset='utf8')
+
+# 데이터베이스를 사용하기 위한 cursor을 세팅.
+cursor = db.cursor()
         
 @app.route('/', methods=['GET'])
 def home():
@@ -11,20 +22,11 @@ def home():
 @app.route('/get_account', methods=['POST'])
 def get_account():
     if request.method == 'POST':
+        global db, cursor
+        
         # JSON 형식으로 데이터 받기
         account_data = request.get_json()
         account_id = account_data['id']
-        
-        # 데이터베이스에 접근
-        db = pymysql.connect(host='localhost',
-                           port=3306,
-                           user='root',
-                           password='12345',
-                           db='friendly_keyboard_accounts',
-                           charset='utf8')
-
-        # 데이터베이스를 사용하기 위한 cursor을 세팅.
-        cursor = db.cursor()
         
         # SQL query 작성
         # SELECT
@@ -45,21 +47,12 @@ def get_account():
 @app.route('/sign-up', methods=['POST'])
 def sign_up():
     if request.method == 'POST':
+        global db, cursor
+        
         # JSON 형식으로 데이터 받기
         account_data = request.get_json()
         account_id = account_data['id']
         password = account_data['password']
-        
-        # 데이터베이스에 접근
-        db = pymysql.connect(host='localhost',
-                           port=3306,
-                           user='root',
-                           password='12345',
-                           db='friendly_keyboard_accounts',
-                           charset='utf8')
-
-        # 데이터베이스를 사용하기 위한 cursor을 세팅.
-        cursor = db.cursor()
         
         # SQL query 작성
         # INSERT
@@ -74,6 +67,33 @@ def sign_up():
         db.commit()
         
         return jsonify({'responseText': 'Success'})
+    
+# 로그인 시 아이디와 비밀번호가 올바른지 확인
+@app.route('/sign-in', method=['POST'])
+def sign_in():
+    if request.method == 'POST':
+        global db, cursor
+        
+        # JSON 형식으로 데이터 받기
+        account_data = request.get_json()
+        account_id = account_data['id']
+        password = account_data['password']
+        
+        # SQL query 작성
+        # SELECT
+        sql = "SELECT * FROM accounts WHERE id = '%s' AND password = '%s'" % (account_id, password)
+        
+        # SQL query 실행
+        cursor.execute(sql)
+        
+        # SQL query 실행 결과를 가져옴
+        result = cursor.fetchone() 
+        
+        if str(type(result)) == "<class 'NoneType'>":
+            return jsonify({'responseText': "Available"})
+        else:
+            return jsonify({'responseText': "Unavailable"})
+        
         
 if __name__ == '__main__':
     app.run('0.0.0.0', port = 5000)
